babe1a191424927d44d2b45fc86ccc8273d41b6a
Fabian Florez <fabiandavid2403@gmail.com>
2020-11-19 15:00:54 -0500

    Funcion creada

--- script.js
+++ script.js
@@ -5,26 +5,28 @@ for(var i=0; i<9; i++) {
     matriz[i] = new Array(9);
 }
 */
+var getData = function(){
 
-var matriz = [
-    new Array(9),new Array(9),new Array(9),
-    new Array(9),new Array(9),new Array(9),
-    new Array(9),new Array(9),new Array(9)
-];
+    var matriz = [
+        new Array(9),new Array(9),new Array(9),
+        new Array(9),new Array(9),new Array(9),
+        new Array(9),new Array(9),new Array(9)
+    ];
 
-var ciclo = 0;
+    var ciclo = 0;
 
-for(var  f = 0; f < 9; f++){
-    for(var c = 0; c < 9; c++){
-        var celda = "f"+f+"c"+c;
-        var valorCelda = document.getElementById(celda).value;
-        matriz[f][c] = valorCelda;     
+    for(var  f = 0; f < 9; f++){
+        for(var c = 0; c < 9; c++){
+            var celda = "f"+f+"c"+c;
+            var valorCelda = document.getElementById(celda).value;
+            matriz[f][c] = valorCelda;     
+        }
+        ciclo++;
     }
-    ciclo++;
+
+    for(var  i = 0; i < matriz.length; i++){
+        for(var j = 0; j < matriz.length; j++){
+            document.writeln(matriz[i][j])
+        }
+    }       
 }
-
-for(var  i = 0; i < matriz.length; i++){
-    for(var j = 0; j < matriz.length; j++){
-        document.writeln(matriz[i][j])
-    }
-}       
